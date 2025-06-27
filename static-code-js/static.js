9470c4f42128438db2b03f83e8f6b3b8822bee9a
euclidstellar <euclidstellar@gmail.com>
2025-06-27 13:12:22 +0530

    removed job level permissions
    Signed-off-by: euclidstellar <euclidstellar@gmail.com>

--- static-code-js/static.js
+++ static-code-js/static.js
@@ -7,8 +7,8 @@ const port = 3000;
 function fetchUser(id) {
     return {
         id,
-        name: 'Bob',
-        email: 'bob@example.com',
+        name: "Bob",
+        email: "bob@example.com",
     };
 }
 
@@ -23,7 +23,7 @@ app.get('/user', (req, res) => {
     return res.json({
         message: `Hello, ${user.name}! Your email is ${user.email}.`,
         user,
-    });
+    })
 });
 
 app.listen(port, () => {
